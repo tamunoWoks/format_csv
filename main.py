926a97cf4825cea34ef72278c0c1f36088d21a9d
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-08 11:15:19 -0600

    Check if the first argument (input file) has a .csv extension

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ def main():
     #Check if number of command_line arguments is correct
     if len(argv) < 3:
         exit('Too few command-line arguments')
-    if len(argv) > 3:
+    elif len(argv) > 3:
         exit('Too many command-line arguments')
+    else:
+        #Check if the first argument (input file) has a .csv extension
+        if not argv[1].endswith('.csv'):
+            exit('Not a CSV file')
+        else:
+            ...
+
 
 
 
