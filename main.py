36f61e66a4bcd813af9b0e0c82f3f165e7296444
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-08 11:12:05 -0600

    In main, check if number of command line arguments is correct

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import csv 
-import sys
+from sys import argv, exit
 
 
 def main():
-    ...
+    #Check if number of command_line arguments is correct
+    if len(argv) < 3:
+        exit('Too few command-line arguments')
+    if len(argv) > 3:
+        exit('Too many command-line arguments')
+
 
 
 if __name__ == '__main__':
