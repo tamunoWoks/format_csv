4b3697c703e18bc2a1342c41e46589191bd423a7
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-08 11:36:24 -0600

    Clean up main.py

--- main.py
+++ main.py
@@ -1,43 +1,42 @@
-import csv 
+import csv
 from sys import argv, exit
 
 
 def main():
-    #Check if number of command_line arguments is correct
+    # Check if number of command_line arguments is correct
     if len(argv) < 3:
-        exit('Too few command-line arguments')
+        exit("Too few command-line arguments")
     elif len(argv) > 3:
-        exit('Too many command-line arguments')
+        exit("Too many command-line arguments")
     else:
-        #Check if the first argument (input file) has a .csv extension
-        if not argv[1].endswith('.csv'):
-            exit('Not a CSV file')
+        # Check if the first argument (input file) has a .csv extension
+        if not argv[1].endswith(".csv"):
+            exit("Not a CSV file")
         else:
             scourge(argv[1], argv[2])
 
 
 def scourge(before, after):
     try:
-        #Open the input CSV file for reading and output CSV file for writing
-        with open(argv[1]) as before, open(argv[2], 'w', newline='') as after:
+        # Open the input CSV file for reading and output CSV file for writing
+        with open(argv[1]) as before, open(argv[2], "w", newline="") as after:
             reader = csv.DictReader(before)
-            header = ['first', 'last', 'house']
+            header = ["first", "last", "house"]
             writer = csv.DictWriter(after, fieldnames=header)
             writer.writeheader()
 
             # Process each row in input file
             for row in header:
                 # Split the 'name' field into last name and first name
-                lname, fname = row['name'].strip().split(', ')
-                house = row['house']
+                lname, fname = row["name"].strip().split(", ")
+                house = row["house"]
                 # Write the processed data to the output file
-                writer.writerow({'first': fname, 'last': lname, 'house': house})
+                writer.writerow({"first": fname, "last": lname, "house": house})
 
     except FileNotFoundError:
-        #Handle file not found error
-        exit(f'Could not read {before}')
+        # Handle file not found error
+        exit(f"Could not read {before}")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
